8e8a35ff2d17d96ba64b348415dded41c9bee327
Yulia <yulia.v@Yulias-MacBook.local>
2022-01-16 19:22:38 +0700

    init: start trello-clone task

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" /> 
+      </header>
+      <h1>Start</h1>
+    </div>
+  );
+}
+
+export default App;
