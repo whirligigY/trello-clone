6d50c7fde12881b91295cd20611763d7a8299442
Taneros <fricentaurus@GMAIL.COM>
2022-01-19 16:25:31 +0300

    init: clone repo clean files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import NavBar from './components/navBar';
-import { Route, Switch } from 'react-router-dom';
-import Boards from './pages/boards';
-import Main from './pages/main';
-import Footer from './components/footer';
+import NavBar from "./components/navBar";
+import { Route, Switch } from "react-router-dom";
+import Boards from "./pages/boards";
+import Main from "./pages/main";
+import Footer from "./components/footer";
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
