c4ce67d8cb24f48fb241938eb6a6489af026cc6f
Taneros <fricentaurus@GMAIL.COM>
2022-02-03 21:20:29 +0300

    fix: sign in center styles

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,11 +1,15 @@
 import "./main.css";
 
-const Main = ({ children }) => (
-  <main className="main">
-    <div id="main" className="main__container">
-      {children}
-    </div>
-  </main>
-);
+const Main = ({ 0: extraClass, children }) => {
+  console.log(`extraClass`, extraClass);
+
+  return (
+    <main className={extraClass ? `main ${extraClass}` : "main"}>
+      <div id="main" className={"main__container"}>
+        {children}
+      </div>
+    </main>
+  );
+};
 
 export default Main;
--- src/components/SignIn/index.jsx
+++ src/components/SignIn/index.jsx
@@ -1,58 +1,13 @@
 import "./signIn.css";
-import { useState, useRef } from "react";
 import Main from "../Main";
-import styled from "@emotion/styled";
-import { Auth, Typography, Button } from "@supabase/ui";
-import { useAuth } from "../../contexts/Auth";
+import { Auth } from "@supabase/ui";
 import { supabase } from "../../client";
 
-const LoginBtn = styled.button`
-  padding-left: 2.5rem;
-  padding-right: 2.5rem;
-`;
-
 export default function SignIn() {
-  const [email, setEmail] = useState("");
-  const [submitted, setSubmitted] = useState(false);
-
-  const { signIn } = useAuth();
-
-  async function submitEmail(e) {
-    e.preventDefault();
-    //TODO
-    /**
-     * add a proper email validation here
-     *
-     **/
-
-    if (!email) return;
-    console.log(`email`, email);
-
-    try {
-      const { error } = await signIn({ email });
-      console.log(`submit email`);
-      if (error) throw new Error(error);
-      else setSubmitted(true);
-    } catch (error) {
-      console.error(error);
-    } finally {
-      console.log("submitted state", submitted);
-    }
-  }
-
-  if (submitted) {
-    console.log(`submitted!`);
-    return (
-      <Main>
-        <div>
-          <h2>Please check your email to get your Magic Link.</h2>
-        </div>
-      </Main>
-    );
-  }
+  const signinClass = "main_center";
 
   return (
-    <Main>
+    <Main {...[signinClass]}>
       <Auth.UserContextProvider supabaseClient={supabase}>
         <Auth supabaseClient={supabase} providers={["google", "github"]} />
       </Auth.UserContextProvider>
