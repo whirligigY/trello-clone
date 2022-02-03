283a9d5dec4433617bd36912c1caac4b15f020e0
Taneros <fricentaurus@GMAIL.COM>
2022-02-03 21:53:35 +0300

    refactor: remove unused code

--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -8,56 +8,26 @@ import { useAuth } from "../../contexts/Auth";
 
 export default function Profile() {
   let history = useHistory();
-  const [profile, setProfile] = useState(null);
 
-  const { user } = useAuth();
+  const { user, signOut } = useAuth();
 
-  useEffect(() => {
-    fetchProfile();
-  }, []);
-
-  async function fetchProfile() {
-    const profileData = await supabase.auth.user();
-    //TODO
-    /**
-     * write proper redirect route
-     **/
-    if (!profileData) {
-      history.push("/sign-in");
-    } else {
-      setProfile(profileData);
-    }
-  }
-
-  if (!profile) return null;
-
-  const Container = (props) => {
-    //TODO: user
-    /**
-     * change to impirted user from use auth
-     **/
-
-    if (user)
-      return (
-        <div className="profile">
-          <div className="profile__info">
-            <Typography.Text>Signed in as: {user.email}</Typography.Text>
+  if (user) {
+    return (
+      <Main>
+        <Auth.UserContextProvider supabaseClient={supabase}>
+          <div className="profile">
+            <div className="profile__info">
+              <Typography.Text>Signed in as: {user.email}</Typography.Text>
+            </div>
+            <Button block onClick={() => signOut()}>
+              Sign out
+            </Button>
           </div>
-          <Button block onClick={() => props.supabaseClient.auth.signOut()}>
-            Sign out
-          </Button>
-        </div>
-      );
-    return props.children;
-  };
-
-  return (
-    <Main>
-      <Auth.UserContextProvider supabaseClient={supabase}>
-        <Container supabaseClient={supabase}>
-          <Auth supabaseClient={supabase} providers={["google", "github"]} />
-        </Container>
-      </Auth.UserContextProvider>
-    </Main>
-  );
+        </Auth.UserContextProvider>
+      </Main>
+    );
+  } else {
+    history.push("/sign-in");
+    return null;
+  }
 }
