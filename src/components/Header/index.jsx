2a6e69c9363ae0f5b9ff72609dc1a30984dafa54
Taneros <fricentaurus@GMAIL.COM>
2022-02-22 00:27:44 +0300

    fix: remove dashboard link from header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react';
+import React, { useContext } from 'react';
 import { Auth, Typography, Button } from '@supabase/ui';
 import { Link } from 'react-router-dom';
 import { useAuth } from '../../contexts/Auth';
@@ -16,12 +16,12 @@ const basicStyles = {
 export const Header = () => {
   const { signOut, user, authState, userProfile } = useAuth();
   const { changeWrapperBg } = useContext(BgContext);
-  const defaultBackground = 'https://fyddxppvkwjfodizkufq.supabase.in/storage/v1/object/public/avatars/beautiful.jpeg';
+  const defaultBackground =
+    'https://fyddxppvkwjfodizkufq.supabase.in/storage/v1/object/public/avatars/beautiful.jpeg';
 
   return (
     <header className="header">
       <nav className="header__nav nav__button">
-
         <Link
           to="/"
           className="nav_button__more"
@@ -29,19 +29,17 @@ export const Header = () => {
           role="button"
         />
         <Link to="/" className="nav_button__trello">
-
           Crello
         </Link>
         <div className="nav__button__sign-in">
           {userProfile()?.aud === 'authenticated' ? (
             <>
-              <Link to="/profile" style={basicStyles}
-              onClick={()=>changeWrapperBg(defaultBackground)}>
+              <Link
+                to="/profile"
+                style={basicStyles}
+                onClick={() => changeWrapperBg(defaultBackground)}
+              >
                 Profile
-              </Link>
-
-              <Link to="/protected" style={basicStyles}>
-                Dashboard
               </Link>
             </>
           ) : null}
