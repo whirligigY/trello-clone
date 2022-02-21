7da20f64463193edad2feec14494bbe259cc9eee
Taneros <fricentaurus@GMAIL.COM>
2022-02-22 00:11:48 +0300

    fix: not found page styles

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Alert } from 'react-bootstrap';
+import './notFound.css';
 
 const NotFound = () => {
   return (
-    <Alert variant="success">
-      <Alert.Heading>404</Alert.Heading>
-      <p>Sorry, page is not found</p>
+    <Alert variant="success" className="not-found">
+      <div className="no-found__alert-msg">
+        <Alert.Heading>404</Alert.Heading>
+        <p>Sorry, page is not found</p>
+      </div>
     </Alert>
   );
 };
