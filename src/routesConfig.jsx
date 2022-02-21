7f5d6a6b474509cfd6a06be90d21a185f7a6b7bb
Yulia <yulia.v@Yulias-MacBook.local>
2022-02-21 21:32:27 +0700

    feat: add 404 page

--- src/routesConfig.jsx
+++ src/routesConfig.jsx
@@ -6,6 +6,7 @@ import { SignIn } from './components/SignIn';
 import { Profile } from './components/Profile';
 import { Header } from './components/Header';
 import { SearchBar } from './components/SearchBar';
+import { NotFound } from './pages/NotFound';
 
 const routesConfig = [
   {
@@ -23,6 +24,10 @@ const routesConfig = [
   {
     path: '/profile',
     element: <Profile />,
+  },
+  {
+    path: '*',
+    element: <NotFound />,
   },
 ];
 
