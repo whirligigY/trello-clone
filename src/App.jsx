2cf42743f3872214a55f9c70b539faa802d863b6
Taneros <fricentaurus@GMAIL.COM>
2022-02-23 15:18:55 +0300

    fix: add protected routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import React, {useState, useEffect} from 'react';
-import { useRoutes } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { useRoutes, useNavigate } from 'react-router-dom';
 import { routesConfig } from './routesConfig';
+import { noLoginRoutesConfig } from './no-login-routesConfig';
 
 import { Header } from './components/Header';
 import Footer from './components/Footer';
-import { AuthProvider } from './contexts/Auth';
+import { AuthProvider, useAuth } from './contexts/Auth';
 import { BgContext } from './contexts/BgContext';
 import { supabase } from './client';
 import './App.css';
@@ -12,28 +13,43 @@ import './App.css';
 // TODO: change switch to route with history
 
 const App = () => {
-  const [wrapperBg, setWrapperBg] = useState('https://fyddxppvkwjfodizkufq.supabase.in/storage/v1/object/public/avatars/beautiful.jpeg')
+  const [wrapperBg, setWrapperBg] = useState(
+    'https://fyddxppvkwjfodizkufq.supabase.in/storage/v1/object/public/avatars/beautiful.jpeg'
+  );
   const routes = useRoutes(routesConfig);
+
+  const noLoginRoute = useRoutes(noLoginRoutesConfig);
 
   const changeWrapperBg = (newBg) => {
     setWrapperBg(newBg);
-  }
+  };
 
-  useEffect(()=>{
-    if (window.location.pathname === '/' || window.location.pathname === '/profile') {
-      setWrapperBg('https://fyddxppvkwjfodizkufq.supabase.in/storage/v1/object/public/avatars/beautiful.jpeg')
+  useEffect(() => {
+    if (
+      window.location.pathname === '/' ||
+      window.location.pathname === '/profile'
+    ) {
+      setWrapperBg(
+        'https://fyddxppvkwjfodizkufq.supabase.in/storage/v1/object/public/avatars/beautiful.jpeg'
+      );
     }
-  }, [window.location.pathname])
+  }, [window.location.pathname]);
 
   return (
     <AuthProvider supabase={supabase}>
-      <BgContext.Provider value={{
-        changeWrapperBg
-      }}>
-        <div className="wrapper"
-        style={{ backgroundImage: `url('${wrapperBg}')` }}>
+      <BgContext.Provider
+        value={{
+          changeWrapperBg,
+        }}
+      >
+        <div
+          className="wrapper"
+          style={{ backgroundImage: `url('${wrapperBg}')` }}
+        >
           <Header />
-          {routes}
+          {supabase.auth.session()?.user?.aud === 'authenticated'
+            ? routes
+            : noLoginRoute}
           <Footer />
         </div>
       </BgContext.Provider>
