ec9599627003275868812f36b30a74c96ce35ac9
Taneros <fricentaurus@GMAIL.COM>
2022-03-15 20:10:57 +0300

    refactor: new password component

--- src/components/NewPass/index.tsx
+++ src/components/NewPass/index.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
-import './newPassword.css';
 import { Auth } from '@supabase/ui';
-import Main from '../Main';
+import { Main } from '../Main';
 import { supabase } from '../../client';
+
+import './newPassword.css';
 
 export const NewPass = () => {
   const signinClass = 'main_center';
 
+  //TODO: remove supabaseClient prop? there is context already...
+
   return (
-    <Main {...[signinClass]}>
+    <Main modClass={signinClass}>
       <Auth.UserContextProvider supabaseClient={supabase}>
         <div className="sbui-auth">
           <div className="sbui-space-col sbui-space-y-8">
